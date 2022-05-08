469fce3ae71b8e76b7a93030d05868f6a83e609c
ulia1706 <102323954+ulia1706@users.noreply.github.com>
2022-05-09 02:10:27 +0300

    #suppliers_info

--- Client/client.cpp
+++ Client/client.cpp
@@ -1,6 +1,6 @@
 ﻿#define _AFXDLL
 //So you must use the DLL version of MFC and the DLL version of the C and C++ 
-//runtime libraries, and link their import libraries, they take on that responsibility.
+//runtime libraries, and link their import libraries, they take on that responsibility
 //And you have to tell the compiler about it, so that the library will go looking for the 
 //right place for those globals.That requires #defining _AFXDLL.Read it like "the application
 //framework lives in its own DLL".The CRT has a macro for that as well, it is _DLL.
@@ -8,6 +8,7 @@
 #pragma comment(lib, "ws2_32.lib")
 #include <winsock2.h>
 #include <iostream>
+#include<conio.h> //для getch
 
 using namespace std;
 
@@ -25,28 +26,178 @@ int CheckNumber()     //пропускает только целые числа
 	}
 }
 
-int CheckChoice(int a, int b) //сужает целые числа до определённого диапазона (от a до b)
+int CheckPlace(char* str) {
+	int flag = 0, k;
+	k = strlen(str);
+		for (int i = 0; i < k; i++) {
+			if ((str[i] >= (-64)) && (str[i] <= (-1)))
+			{
+				flag++;
+			}
+			if ((str[i] == (-88)) || (str[i] == (-72)))
+			{
+				flag++;
+			}
+			if ((str[i] == 45) || (str[i] == 32)) {
+				flag++;
+			}
+		}
+		if (flag != k) return 1;
+		else return 2;
+}
+
+int CheckPhone(char* str) {
+	int flag = 0, k;
+	k = strlen(str);
+	for (int i = 0; i < k; i++) {
+		if ((str[i] >= (48)) && (str[i] <= (57)))
+		{
+			flag++;
+		}
+		if ((str[i] == (41)) || (str[i] == (40)))
+		{
+			flag++;
+		}
+		if ((str[i] == 43) || (str[i] == 32)) {
+			flag++;
+		}
+		if (str[i] == 45) {
+			flag++;
+		}
+	}
+	if (flag != k) return 1;
+	else return 2;
+}
+
+int CheckEmail(char* str) {
+	int flag = 0, k,g=0;
+	k = strlen(str);
+	for (int i = 0; i < k; i++) {
+		if ((str[i] >= 65) && (str[i] <= 90)) //большие латинские
+		{
+			flag++;
+		}
+		if ((str[i] >= 97) && (str[i] <= 122)) //маленькие латинские
+		{
+			flag++;
+		}
+		if ((str[i] >= (48)) && (str[i] <= (57))) //цифры
+		{
+			flag++;
+		}
+		if ((str[i] == '-') || (str[i] == '_')) {
+			flag++;
+		}
+		if ((str[i] == '@') || (str[i] == '.')) {
+			flag++;
+			g++;
+		}
+	}
+	if ((flag != k)||(g!=2)) return 1;
+	else return 2;
+}
+
+float CheckFloat()     //пропускает дробные числа и числа с плавающей точкой
 {
-	int choice = 0;
+	float number;
+	while (1) {
+		cin >> number;
+		if (cin.get() != '\n') {
+			cout << "Это не похоже на целое или дробное число... Повторите попытку : " << endl;
+			cin.clear();
+			cin.ignore(32767, '\n');
+		}
+		else return number;
+	}
+}
+
+float CheckChoiceF(float a, float b) //сужает целые числа до определённого диапазона (от a до b)
+{
+	float choice = 0;
 	while (choice == 0) {
-		choice = CheckNumber();
+		choice = CheckFloat();
 		if ((choice < a) || (choice > b)) {
-			cout << "Некорректный выбор! Пожалуйста, повторите попытку:" << endl;
+			cout << "Такой вариант невозможен! Пожалуйста, повторите попытку:" << endl;
 			choice = 0;
 		}
 	}
 	return choice;
 }
 
-bool check(char* str)
+int CheckChoice(int a, int b) //сужает целые числа до определённого диапазона (от a до b)
 {
-	for (int i = 0; str[i] != '\0'; i++)
-		if (str[i] == '!' || str[i] == '@' || str[i] == '#' || str[i] == '$' || str[i] == '%' || str[i] == '^' || str[i] == '&' || str[i] == '*' || str[i] == '/' || str[i] == '.')
-			return false;
-	return true;
+	int choice = 0;
+	while (choice == 0) {
+		choice = CheckNumber();
+		if ((choice < a) || (choice > b)) {
+			cout << "Такой вариант невозможен! Пожалуйста, повторите попытку:" << endl;
+			choice = 0;
+		}
+	}
+	return choice;
 }
 
-int CheckLetters(char stroka[100])
+int CheckLogin(char* str) {
+	int flag = 0, k;
+	k = strlen(str);
+	for (int i = 0; i < k; i++) {
+		if ((str[i] >= 65) && (str[i] <= 90)) //большие латинские
+		{
+			flag++;
+		}
+		if ((str[i] >= 97) && (str[i] <= 122)) //маленькие латинские
+		{
+			flag++;
+		}
+		if ((str[i] >= (48)) && (str[i] <= (57))) //цифры
+		{
+			flag++;
+		}
+		if ((str[i] == '.') || (str[i] == '_')) {
+			flag++;
+		}
+	}
+	if ((strlen(str) > 16) || (strlen(str) < 6)) {
+		cout << "Логин должен содержать от 6 до 16 символов.\nПожалуйста, попробуйте снова:" << endl;
+		return 0;
+	}
+	if (flag != k) return 1;
+	else return 2;
+}
+
+int CheckPassword(char* str) {
+	int flag = 0, k;
+	k = strlen(str);
+	if ((strlen(str) > 18) || (strlen(str) < 8)) {
+		cout << "Пароль должен содержать от 8 до 18 символов.\nПожалуйста, попробуйте снова:" << endl;
+		return 0;
+	}
+	for (int i = 0; i < k; i++) {
+		if ((str[i] >= (-64)) && (str[i] <= (-1)))
+		{
+			flag++;
+		}
+		if ((str[i] == (-88)) || (str[i] == (-72)))
+		{
+			flag++;
+		}
+		if ((str[i] == 45) || (str[i] == 32)) {
+			flag++;
+		}
+	}
+	if (flag != 0) return 1;
+	else return 2;
+}
+
+//bool check(char* str)
+//{
+//	for (int i = 0; str[i] != '\0'; i++)
+//		if (str[i] == '!' || str[i] == '@' || str[i] == '#' || str[i] == '$' || str[i] == '%' || str[i] == '^' || str[i] == '&' || str[i] == '*' || str[i] == '/' || str[i] == '.')
+//			return false;
+//	return true;
+//}
+
+int CheckLetters(char stroka[100]) //добавить ещё английские буковки может???
 {
 	int k, flag = 0;
 	k = strlen(stroka);
@@ -110,6 +261,7 @@ int main() {
 		send(s, b, sizeof(b), 0); //отправили на сервер
 		recv(s, k, sizeof(k), 0);
 		t = atoi(k);   //число отвечающее за номер кейса в соответствии с главным меню
+		system("cls");
 		switch (t) {
 		case 1: {
 			recv(s, k, sizeof(k), 0);
@@ -249,20 +401,174 @@ int main() {
 				send(s, b, sizeof(b), 0);
 				recv(s, k, sizeof(k), 0);
 				t1 = atoi(k);
+				system("cls");
 				switch (t1) {
 				case 1: {
-					cout << "Введите название организации\n";
 					int a = 0;
-					while (a != 2) {
+					float c =0;
+					//вводим название организации
+						cout << "Введите название вашей организации:  ";
 						cin.getline(buf, 50, '\n');
-						a=CheckLetters(buf);
-						if (a == 1) { cout << "В данном поле могут быть использованы только буквы!" << endl; }
+						send(s, buf, sizeof(buf), 0);
+					
+					//вводим страну локации поставщика
+					cout << "Введите страну локации поставщика:  ";
+					while (a != 2) {
+						buf[0] = '\0';
+						cin.getline(buf, 50, '\n');
+						a = CheckPlace(buf);
+						if (a == 1) { cout << "Здесь могут быть только буквы кириллицы, дефис и пробел! Пожалуйста, попробуйте снова:" << endl; }
 					}
 					send(s, buf, sizeof(buf), 0);
-					cout << "Введите номер телефона\n";
-					cout << "Введите электронную почту\n";
-					cout << "Введите количество действующих контрактов\n";
-					cout << "Введите минимальную цену разовой закупки\n";
+					a = 0;
+					//вводим город локации поставщика
+					cout << "Введите город локации поставщика:  ";
+					while (a != 2) {
+						buf[0] = '\0';
+						cin.getline(buf, 50, '\n');
+						a = CheckPlace(buf);
+						if (a == 1) { cout << "Здесь могут быть только буквы кириллицы, дефис и пробел! Пожалуйста, попробуйте снова:" << endl; }
+					}
+					send(s, buf, sizeof(buf), 0);
+					a = 0;
+					//вводим фамилию представителя
+					cout << "Введите фамилию представителя:  ";
+					while (a != 2) {
+						buf[0] = '\0';
+						cin.getline(buf, 50, '\n');
+						a = CheckLetters(buf);
+						if (a == 1) { cout << "В данном поле могут быть использованы только буквы кириллицы!" << endl; }
+					}
+					send(s, buf, sizeof(buf), 0);
+					a = 0;
+					//вводим имя представителя
+					cout << "Введите имя представителя:  ";
+					while (a != 2) {
+						buf[0] = '\0';
+						cin.getline(buf, 50, '\n');
+						a = CheckLetters(buf);
+						if (a == 1) { cout << "В данном поле могут быть использованы только буквы кириллицы!" << endl; }
+					}
+					send(s, buf, sizeof(buf), 0);
+					a = 0;
+					//вводим отчество представителя
+					cout << "Введите отчество представителя:  ";
+					while (a != 2) {
+						buf[0] = '\0';
+						cin.getline(buf, 50, '\n');
+						a = CheckLetters(buf);
+						if (a == 1) { cout << "В данном поле могут быть использованы только буквы кириллицы!" << endl; }
+					}
+					send(s, buf, sizeof(buf), 0);
+					a = 0;
+					//вводим номер телефона представителя
+					cout << "Введите номер телефона:  ";
+					while (a != 2) {
+						buf[0] = '\0';
+						cin.getline(buf, 50, '\n');
+						a = CheckPhone(buf);
+						if (a == 1) { cout << "Кажется, номер телефона введён некорректно... Пожалуйста, попробуйте снова:" << endl; }
+					}
+					send(s, buf, sizeof(buf), 0);
+					a = 0;
+					//вводим электронную почту
+					cout << "Введите электронную почту:  ";
+					while (a != 2) {
+						buf[0] = '\0';
+						cin.getline(buf, 50, '\n');
+						a = CheckEmail(buf);
+						if (a == 1) { cout << "Кажется, адрес электронной почты введён некорректно...\nПожалуйста, попробуйте снова:" << endl; }
+					}
+					send(s, buf, sizeof(buf), 0);
+					a = 0;
+					buf[0] = '\0';
+					//вводим количество действующих контрактов
+					cout << "Введите количество действующих контрактов:  ";
+					a = CheckChoice(1, 100000);
+					_itoa_s(a, buf, 10);
+					send(s, buf, sizeof(buf), 0);
+					a = 0;
+					buf[0] = '\0';
+					//вводим минимальную сумму разовой закупки
+					cout << "Введите минимальную сумму разовой закупки:  ";
+					c = CheckChoiceF(1, 1000000000);
+					sprintf_s(buf, "%.2f", c);
+					send(s, buf, sizeof(buf), 0);
+					a = 0;
+					buf[0] = '\0';
+					////вводим информацию об имеющихся договорах с заказчиками
+					//int choice,cho;
+					//cout << "Введите данные о заключённых с заказчиками договорах." << endl;
+					//cout << "1. Перейти к следующему шагу.\n2. Ввести данные." << endl;
+					//choice = CheckChoice(1, 2);
+					//cho = choice;
+					//int ch1 = 0;
+					//buf[0] = '\0';
+					//_itoa_s(cho, buf, 10);
+					//send(s, buf, sizeof(buf), 0);
+					//if (choice == 2) {
+					//	while (ch1 != 1) {
+					//		cout << "Введите название организации-заказчика:  ";
+					//		buf[0] = '\0';
+					//		cin.getline(buf, 50, '\n');
+					//		send(s, buf, sizeof(buf), 0);
+					//		cout << "Введите номер договора:  ";
+					//		buf[0] = '\0';
+					//		cin.getline(buf, 50, '\n');
+					//		send(s, buf, sizeof(buf), 0);
+					//		if (ch1 != 1) { cout << "1. Перейти к следующему шагу.\n2. Продолжить ввод данных." << endl; ch1 = CheckChoice(1, 2); }
+					//		cho = ch1;
+					//		buf[0] = '\0';
+					//		_itoa_s(cho, buf, 10);
+					//		send(s, buf, sizeof(buf), 0);
+					//	}
+					//}
+					//задаём логин
+					int ch = 0;
+					while (ch != -1) {
+						cout << "Придумайте логин:  ";
+						while (a != 2) {
+							buf[0] = '\0';
+							cin.getline(buf, 50, '\n');
+							a = CheckLogin(buf);
+							if (a == 1) { cout << "Были использованы недопустимые символы. Повторите попытку:" << endl; }
+						}
+						send(s, buf, sizeof(buf), 0);
+						k[0] = '\0';
+						recv(s, k, sizeof(k), 0);
+						ch = atoi(k);
+						cout << ch << endl;
+						k[0] = '\0';
+						a = 0;
+						buf[0] = '\0';
+					}
+					
+					//задаём пароль
+					int j = 0;
+					cout << "Придумайте пароль:  ";
+					a = 0;
+					while (a != 2) {
+						buf[0] = '\0';
+						for (;;) {
+							buf[j] = _getch();
+							if (buf[j] == '\r')
+								break;
+							if (buf[j] == '\b') {
+								cout << "\b \b";
+								j--;
+							}
+							else if (buf[j] != '\0') {
+								cout << "*";
+								j++;
+							}
+						}
+						buf[j] = '\0';
+						//cin.getline(buf, 50, '\n');
+						a = CheckPassword(buf);
+						if (a == 1) { cout << "Были использованы недопустимые символы. Повторите попытку:" << endl; }
+					}
+					send(s, buf, sizeof(buf), 0);
+					a = 0;
 					break;
 				}
 				case 2: {
--- Server/server.cpp
+++ Server/server.cpp
@@ -3,7 +3,7 @@
 #include <afx.h>  //для ускорения компиляции проектов
 #include <winsock2.h>
 #include <process.h> /* _beginthread, _endthread */  //содержит объявления функций и макросы, используемые при работе с потоками и процессами
-#include <string.h>
+#include <string>
 #include <fstream>
 #include <list>
 
@@ -11,11 +11,42 @@ using namespace std;
 CFile f;  //Базовый класс для файловых классов Microsoft Foundation Class. Создает CFileобъект из пути или дескриптора файла
 CFileException ex;
 
+class Contract {
+	char client[50];     //заказчик
+	char supplier[50];   //поставщик
+	char contr_num[50];  //номер договора
+	int status;          //статус договора
+public:
+	Contract() {
+		strcpy_s(client, "АвтоМир");
+		strcpy_s(supplier, "-");
+		strcpy_s(contr_num, "-");
+		status = 0;
+	}
+	//void SetClient(char* str) { strcpy_s(client, str); }
+	void SetSupplier(char* str) { strcpy_s(supplier, str); }
+	void SetContrNum(char* str) { strcpy_s(contr_num, str); }
+	void SetStatus(int a) { status = a; }
+}; //информация и методы, связанные с контрактами для обеих сторон,
+//этот класс будет включен в виде списка в класс админ/салон и класс поставщик
+
 class Authorization {
 protected:
-	char login[20];
-	char password[20];
+	char login[50];
+	char password[50];
 public:
+	Authorization() {
+		strcpy_s(login, "-");
+		strcpy_s(password, "-");
+	}
+	/*Authorization(const Authorization& obj) {
+		strcpy_s(this->login,obj.login);
+		strcpy_s(this->password, obj.password);
+	}*/
+	void SetLogin(char* str) { strcpy_s(login, str); }
+	void SetPassword(char* str) { strcpy_s(password, str); }
+	char* GetLogin() { return login; }
+	char* GetPassword() { return password; }
 	int CheckLogin(); //возвращает 1, если всё правильно, 2 - если неправильно (к примеру) //проверка правильности логина
 	int CheckPassword(); //проверка правильности пароля
 	void ChangeLogin(); //изменить логин
@@ -30,9 +61,24 @@ protected:
 	char I[50]; //имя сотрудника
 	char O[50]; //отчество
 public:
-	void SetF(); //ввести фамилию
-	void SetI(); //ввести имя
-	void SetO(); //ввести отчество
+	CarShowroom() {
+		strcpy_s(name, "-");
+		strcpy_s(F, "-");
+		strcpy_s(I, "-");
+		strcpy_s(O, "-");
+	}
+	/*CarShowroom(const CarShowroom& obj) {
+		strcpy_s(this->name, obj.name);
+		strcpy_s(this->F, obj.F);
+		strcpy_s(this->I, obj.I);
+		strcpy_s(this->O, obj.O);
+	}*/
+	void SetF(char* str) { strcpy_s(F, str); } //ввести фамилию
+	void SetI(char* str) { strcpy_s(I, str); } //ввести имя
+	void SetO(char* str) { strcpy_s(O, str); }; //ввести отчество
+	char* GetF() { return F; }
+	char* GetI() { return I; }
+	char* GetO() { return O; }
 };
 
 class Admin :public CarShowroom {
@@ -60,42 +106,152 @@ bool operator<(Expert e1, Expert e2) {
 }
 
 class Supplier :public CarShowroom {
-	char name[50]; //название организации
-	char phone[20]; //контактный телефон
+	char nameorg[50]; //название организации
+	char country[50]; //название страны локации поставщика
+	char city[50]; //название города локации поставщика
+	char phone[50]; //контактный телефон
 	char email[50]; //электронная почта
 	int contracts; //количество действующих договоров
 	//список договоров
 	//список заявок
 	float min_price; //цена минимальной разовой закупки
+	Contract c;      //договор с нашим автосалоном
 public:
 	Supplier() {
-		strcpy_s(name, "-");
+		strcpy_s(nameorg, "-");
+		strcpy_s(country, "-");
+		strcpy_s(city, "-");
 		strcpy_s(phone, "-");
 		strcpy_s(email, "-");
 		contracts = 0;
 		min_price = 0;
 	}
+	/*Supplier(const Supplier& obj) {
+		strcpy_s(this->nameorg, obj.nameorg);
+		strcpy_s(this->country, obj.country);
+		strcpy_s(this->city, obj.city);
+		strcpy_s(this->F, obj.F);
+		strcpy_s(this->I, obj.I);
+		strcpy_s(this->O, obj.O);
+		strcpy_s(this->phone, obj.phone);
+		strcpy_s(this->email, obj.email);
+		this->contracts = obj.contracts;
+		this->min_price = obj.min_price;
+	}*/
 	friend bool operator<(Supplier s1, Supplier s2);
-	//методы поставщика
+	friend void MakeSuppList(list<Supplier>& lst);
+	void Set_nameorg(char* str) { strcpy_s(nameorg, str); }
+	void Set_country(char* str) { strcpy_s(country,str); }
+	void Set_city(char* str) { strcpy_s(city, str); }
+	void Set_phone(char* str) { strcpy_s(phone, str); }
+	void Set_email(char* str){ strcpy_s(email, str); }
+	void Set_contracts(int a) { contracts = a; }
+	void Set_min_price(float a) { min_price = a; }
+	char* Get_nameorg() { return nameorg; }
+	char* Get_country() { return country; }
+	char* Get_city() { return city; }
+	char* Get_phone() { return phone; }
+	char* Get_email() { return email; }
+	int Get_contracts() { return contracts; }
+	float Get_min_price() { return min_price; }
+	void WriteFile(list<Supplier> lst);
 };
+
+void Supplier::WriteFile(list<Supplier> lst)
+{
+	list<Supplier>::iterator p;
+	ofstream file("Suppliers.txt", ios_base::trunc);
+	for (p=lst.begin(); p!=lst.end(); p++) {
+		file << p->nameorg << ";" << p->country << ";" <<p->city << ";" << p->F << ";" << p->I << ";" <<p->O<<";" <<p->phone <<";" << p->email << ";" << p->contracts << ";" << p->min_price << "\n";
+	}
+	file << "*;";
+	file.close();
+	ofstream f("Supp_log_passw.txt", ios_base::trunc);
+	for (p = lst.begin(); p != lst.end(); p++) {
+		for (int i = 0; i < strlen(p->login); i++) {
+			p->login[i] = (char)((int)p->login[i] + 3);
+		}
+		for (int i = 0; i < strlen(p->password); i++) {
+			p->password[i] = (char)((int)p->password[i] + 3);
+		}
+		f << p->login << " " << p->password<< "\n";
+	}
+	f.close();
+}
 
 bool operator<(Supplier s1, Supplier s2) {
 	return strcmp(s1.F, s2.F) < 0;
 }
 
-class Contract{}; //информация и методы, связанные с контрактами для обеих сторон,
-//этот класс будет включен в виде списка в класс админ/салон и класс поставщик
+
 
 class Request{}; //инфа и методы о заявках для обеих сторон
 
 class Method{}; //поля и методы, связанные с методом ранга, включён в класс эксперты
 
+void MakeSuppList(list<Supplier>& lst) {
+	ifstream file("Suppliers.txt");
+	ifstream f("Supp_log_passw.txt");
+	char str[50];
+	Supplier sup;
+	if (!file.is_open()) { //проверяем, связан ли наш поток с открываемым файлом
+	cout << "Возникла проблема при открытии файла\n" << endl;
+}
+else {
+		cout << "File is open ok" << endl;
+	for (int i = 0;; i++) 
+	{
+		file.getline(sup.nameorg, 50, ';');
+		if (strcmp(sup.nameorg, "*") == 0) { break; }
+		file.getline(sup.country, 50, ';');
+		file.getline(sup.city, 50, ';');
+		file.getline(sup.F, 50, ';');
+		file.getline(sup.I, 50, ';');
+		file.getline(sup.O, 50, ';');
+		file.getline(sup.phone, 50, ';');
+		file.getline(sup.email, 50, ';');
+		str[0] = '\0';
+		file.getline(str, 50, ';');
+		sup.Set_contracts(atoi(str));
+		str[0] = '\0';
+		file.getline(str, 50, '\n');
+		sup.Set_min_price(stof(str));
+		str[0] = '\0';
+		f.getline(str, 50, ' ');
+		for (int i = 0; i < strlen(str); i++) {
+			str[i] = (char)((int)str[i] - 3);
+		}
+		strcpy_s(sup.login, str);
+		str[0] = '\0';
+		f.getline(str, 50, '\n');
+		for (int i = 0; i < strlen(str); i++) {
+			str[i] = (char)((int)str[i] - 3);
+		}
+		strcpy_s(sup.password, str);
+		str[0] = '\0';
+		lst.push_back(sup);
+	}
+}
+file.close();
+f.close();
+}
+
 void mailWorking(void* newS) {
+	Supplier supp; 
+	list<Supplier> lsupp;
+	list<Supplier> ::iterator psupp;
 	int c, c1 = 0, c2 = 0, c3 = 0;
 	int flag = 0;
-	char p[500], com[200], k[500], m[500];
+	char p[500], com[500], k[500], m[500];
 	com[0] = '\0'; p[0] = '\0'; k[0] = '\0'; m[0] = '\0';
 	cout << "Сервер работает.\n";
+	/*cout <<  "size = " << lsupp.size() << endl;*/
+	MakeSuppList(lsupp);
+	//cout << lsupp.size() << endl;
+	cout << "фУНКЦИЯ ЗАВЕРШЕНА МЭЙКСАПЛИСТ" << endl;
+	for (psupp = lsupp.begin(); psupp != lsupp.end(); psupp++) {
+		cout << psupp->GetPassword() << endl;
+	}
 	while (1) {
 		strcpy_s(p, "ГЛАВНОЕ МЕНЮ:\n");
 		send((SOCKET)newS, p, sizeof(p), 0);
@@ -257,7 +413,7 @@ void mailWorking(void* newS) {
 			send((SOCKET)newS, p, sizeof(p), 0);
 			p[0] = '\0';
 			while (c1 != 6) {
-				strcpy_s(k, "МЕНЮ ПОСТАВЩИКА:\n 1. Ввести информацию.\n 2. Отредактировать информацию.\n 3. Удалить информацию.\n 4. Заключить договор.\n 5. Расторгнуть договор.\n 6. Вернуться в главное меню.\n ");
+				strcpy_s(k, "МЕНЮ ПОСТАВЩИКА:\n 1. Ввести информацию.\n 2. Отредактировать информацию.\n 3. Удалить информацию.\n 4. Заключить договор.\n 5. Расторгнуть договор.\n 6. Вернуться в главное меню.");
 				send((SOCKET)newS, k, sizeof(k), 0);
 				recv((SOCKET)newS, m, sizeof(m), 0);
 				c1 = atoi(m);
@@ -265,8 +421,106 @@ void mailWorking(void* newS) {
 				case 1: {
 					strcpy_s(p, "1");
 					send((SOCKET)newS, p, sizeof(p), 0);
-					recv((SOCKET)newS, com, sizeof(m), 0);
-					cout << com << endl;
+
+					//получение названия организации
+					recv((SOCKET)newS, com, sizeof(com), 0);
+					supp.Set_nameorg(com);
+					com[0] = '\0';
+
+					//получение страны
+					recv((SOCKET)newS, com, sizeof(com), 0);
+					supp.Set_country(com);
+					com[0] = '\0';
+
+					//получение города
+					recv((SOCKET)newS, com, sizeof(com), 0);
+					supp.Set_city(com);
+					com[0] = '\0';
+
+					//получение фамилии
+					recv((SOCKET)newS, com, sizeof(com), 0);
+					supp.SetF(com);
+					com[0] = '\0';
+
+					//получение имя
+					recv((SOCKET)newS, com, sizeof(com), 0);
+					supp.SetI(com);
+					com[0] = '\0';
+
+					//получение отчества
+					recv((SOCKET)newS, com, sizeof(com), 0);
+					supp.SetO(com);
+					com[0] = '\0';
+
+					//получение номера телефона
+					recv((SOCKET)newS, com, sizeof(com), 0);
+					supp.Set_phone(com);
+					com[0] = '\0';
+
+					//получение электронную почту
+					recv((SOCKET)newS, com, sizeof(com), 0);
+					supp.Set_email(com);
+					com[0] = '\0';
+
+					//получение количество действующих контрактов
+					recv((SOCKET)newS, com, sizeof(com), 0);
+					int d;
+					d = atoi(com);
+					supp.Set_contracts(d);
+					com[0] = '\0';
+
+					//получение минимальную стоимость
+					recv((SOCKET)newS, com, sizeof(com), 0);
+					float numb;
+					numb = stof(com);
+					supp.Set_min_price(numb);
+					com[0] = '\0';
+
+					////получение информации о договорах
+					//int ss,fll=0;
+					//recv((SOCKET)newS, com, sizeof(com), 0);
+					//ss = atoi(com);
+					//if (ss == 2) {
+					//	while (fll != 1) {
+					//		com[0] = '\0';
+					//		recv((SOCKET)newS, com, sizeof(com), 0);
+					//		supp.Set
+					//	}
+					//}
+					//com[0] = '\0';
+					//получение логина
+					int ch = 0;
+					while (ch != -1) {
+						ch = 0;
+						recv((SOCKET)newS, com, sizeof(com), 0);
+						for (psupp = lsupp.begin(); psupp != lsupp.end(); psupp++) {
+							if (strcmp(com, psupp->GetLogin()) == 0) {
+								ch++;
+							}
+						}
+						if (ch == 0) {
+							ch = -1;
+						}
+							m[0] = '\0';
+							_itoa_s(ch, m, 10);
+							send((SOCKET)newS, m, sizeof(m), 0);
+							m[0] = '\0';
+							
+					}
+					supp.SetLogin(com);
+					com[0] = '\0';
+
+					//получение пароль
+					recv((SOCKET)newS, com, sizeof(com), 0);
+					supp.SetPassword(com);
+					com[0] = '\0';
+
+					lsupp.push_back(supp);
+					for (psupp = lsupp.begin(); psupp != lsupp.end(); psupp++) {
+						cout << psupp->Get_nameorg() << "\n" << psupp->Get_country() << "\n" << psupp->Get_city() << "\n" << psupp->GetF() << "\n" << psupp->GetI() << "\n" << psupp->GetO() << "\n" << psupp->Get_phone() << "\n" << psupp->Get_email() << "\n" << psupp->Get_contracts() << "\n" << psupp->Get_min_price() << endl;
+						cout << psupp->GetLogin() << "\n" << psupp->GetPassword() << endl;
+					}
+					supp.WriteFile(lsupp);
 					break;
 				}
 				case 2: {
@@ -403,3 +657,5 @@ int main() {
 	WSACleanup();  //прекращаем работу Winsock API
 	return 0;
 }
+
+
